497fef10a1533afd18f4f55bee321cd033d97e15
DEMONIZED-PC\demonized <themrdemonized@gmail.com>
2023-06-28 21:27:18 +1000

    title for pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,33 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, TitleStrategy } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { Title } from '@angular/platform-browser';
+import { Injectable } from '@angular/core';
+import { RouterStateSnapshot } from '@angular/router';
+
+@Injectable({providedIn: 'root'})
+export class MEXCTitleStrategy extends TitleStrategy {
+  constructor(private readonly title: Title) {
+    super();
+  }
+  
+  override updateTitle(routerState: RouterStateSnapshot) {
+    const title = this.buildTitle(routerState);
+    this.title.setTitle(title ? `MEXCAPI - ${title}` : `MEXCAPI`);
+  }
+}
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent, pathMatch: 'full' },
-  { path: '**', component: PageNotFoundComponent }
+  { path: '', component: HomePageComponent, pathMatch: 'full', title: 'Home' },
+  { path: '**', component: PageNotFoundComponent, title: '404' }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    {provide: TitleStrategy, useClass: MEXCTitleStrategy}
+  ]
 })
 export class AppRoutingModule { }
